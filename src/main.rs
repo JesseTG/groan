fafb75965f26b8b5489bb257ff91699c54723807
Jesse Talavera <jesse@jesse.tg>
2024-07-22 17:59:44 -0400

    Write a skeleton of a program
    - It just parses command-line args and prints the OpenAI key right now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+// NOTE: These doc comments are parsed and embedded into the CLI itself.
+
+/// groan - Good RetroArch OpenAI iNtegration
+#[derive(Parser, Debug)]
+#[command(version, about, long_about)]
+struct Cli {
+    /// The API key used to authenticate with OpenAI.
+    /// Provide on the command-line or with the OPENAI_API_KEY environment variable.
+    #[arg(short, long, env="OPENAI_API_KEY")]
+    key: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+
+    println!("{}", cli.key);
+
+    // TODO: Serve up "hello world" in-browser
 }
