f097569b7975de58972f2033a4e05b960298531b
Jesse Talavera <jesse@jesse.tg>
2024-07-23 11:45:40 -0400

    Flesh out the base a little
    - Add logging
    - Add a parameter object
    - Allow the port to be set

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use serde::{Deserialize, Serialize};
+use warp::Filter;
 
 // NOTE: These doc comments are parsed and embedded into the CLI itself.
 
@@ -8,15 +10,35 @@ use clap::Parser;
 struct Cli {
     /// The API key used to authenticate with OpenAI.
     /// Provide on the command-line or with the OPENAI_API_KEY environment variable.
-    #[arg(short, long, env="OPENAI_API_KEY")]
+    #[arg(short, long, env = "OPENAI_API_KEY")]
     key: String,
+
+    #[arg(short, long, default_value_t = 4404)]
+    port: u16,
+
+    // TODO: Select a host
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct RequestParams {
+    source_lang: Option<String>,
+    target_lang: Option<String>,
+    output: String,
 }
 
 #[tokio::main]
 async fn main() {
+
     let cli = Cli::parse();
+    pretty_env_logger::init();
 
-    println!("{}", cli.key);
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path::end() // Only match the root
+        .and(warp::query::<RequestParams>())
+        .map(|name| format!("Hello, {:?}!", name))
+        .with(warp::log("groan::api"));
 
-    // TODO: Serve up "hello world" in-browser
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], cli.port))
+        .await;
 }
