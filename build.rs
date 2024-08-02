6ab590803638dd9a877dffa3c240015bcab2d8ca
Jesse Talavera <jesse@jesse.tg>
2024-08-02 12:33:48 -0400

    Add tsconfig.json

--- build.rs
+++ build.rs
@@ -3,4 +3,5 @@ use npm_rs::*;
 fn main() {
     println!("cargo::rerun-if-changed=assets"); // Assets are embedded in the binary
     println!("cargo::rerun-if-changed=package.json"); // package.json controls how the frontend is built
+    println!("cargo::rerun-if-changed=tsconfig.json");
 }
