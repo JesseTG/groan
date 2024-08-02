1b62e376b2718722f9ade916fe7b89f49f986556
Jesse Talavera <jesse@jesse.tg>
2024-08-02 11:36:43 -0400

    Add a stub of a build script

---
+++ build.rs
@@ -0,0 +1,6 @@
+use npm_rs::*;
+
+fn main() {
+    println!("cargo::rerun-if-changed=assets"); // Assets are embedded in the binary
+    println!("cargo::rerun-if-changed=package.json"); // package.json controls how the frontend is built
+}
