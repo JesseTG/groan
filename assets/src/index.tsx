cd556424c6f4c405f190aa7522f50dbb2a6e23ed
Jesse Talavera <jesse@jesse.tg>
2024-08-02 12:35:50 -0400

    Build the frontend as part of `cargo build`

---
+++ assets/src/index.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { createRoot } from "react-dom/client";
+import { Button, Dialog, DialogHeading } from "@ariakit/react";
+
+function App() {
+    const [open, setOpen] = useState(false);
+    return (
+        <>
+            <Button onClick={() => setOpen(true)}>Open dialog</Button>
+            <Dialog open={open} onClose={() => setOpen(false)}>
+                <DialogHeading>Ariakit</DialogHeading>
+                <p>Welcome to Ariakit!</p>
+            </Dialog>
+        </>
+    );
+}
--- build.rs
+++ build.rs
@@ -1,7 +1,20 @@
 use npm_rs::*;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo::rerun-if-changed=assets"); // Assets are embedded in the binary
     println!("cargo::rerun-if-changed=package.json"); // package.json controls how the frontend is built
     println!("cargo::rerun-if-changed=tsconfig.json");
+
+    let exit_status = NpmEnv::default()
+        .with_node_env(&NodeEnv::from_cargo_profile().unwrap_or_default())
+        .init_env()
+        .install(None)
+        .run("build")
+        .exec()?;
+
+    if !exit_status.success() {
+        return Err("Failed to build the frontend".into());
+    }
+
+    Ok(())
 }
